2e9376515716cc9da49e144b92082dbdaa02fddc
minseok <alstjr0311@naver.com>
2023-09-14 14:13:49 +0900

    재배포하기위한 변경

--- src/main/java/com/soccer/common/FileManagerService.java
+++ src/main/java/com/soccer/common/FileManagerService.java
@@ -20,7 +20,7 @@ public class FileManagerService {
 		private TextTransService textTransService;
 	
 	
-		public static final String FILE_UPLOAD_PATH = "E:\\7_FinalProject\\workspace\\images/";
+		public static final String FILE_UPLOAD_PATH = "/home/ec2-user/images/";
 		
 		
 		// 이미지 파일 저장 
